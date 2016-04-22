6afc164d64ee2baee20c24783bfd3066f93d7573
sioanis <sueaannn@gmail.com>
2016-04-21 22:46:03 -0700

    Adding some mock copy and animations.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,13 @@
-console.log('\'Allo \'Allo!');
+$(function() {
+	$('.slide').waypoint(function(direction) {
+    if ($(this.element).index() === 2) {
+      $(this.element).addClass('bounceInRight');       
+    } else {
+      $(this.element).addClass('bounceInUp');      
+    }
+
+    setTimeout(function() {
+      $('.slide').removeClass('bounceInUp bounceInRight');
+    }, 1000);
+	});
+});
