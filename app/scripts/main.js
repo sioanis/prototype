b58d168c86f6673caea4d7a3a1e59f377b2f6a1c
sioanis <sueann.ioanis@gmail.com>
2016-04-22 12:59:06 -0700

    Updates to animation.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,48 @@
 $(function() {
-	$('.slide').waypoint(function(direction) {
-    if ($(this.element).index() === 2) {
-      $(this.element).addClass('bounceInRight');       
-    } else {
-      $(this.element).addClass('bounceInUp');      
+    var currentSlide = 1;
+
+    var throttled = _.throttle(direction, 1500); 
+
+    $('.slide').on('mousewheel', throttled);
+
+    function direction(e) {
+        
+      var deltaY = e.originalEvent.deltaY;
+
+      if(deltaY < 0){
+        console.log('up' + currentSlide)
+        
+        switch (currentSlide) {
+            case 2: 
+                $('.slide:nth-child(1)').removeClass('bounceOutUp');
+                $('.slide:nth-child(1)').addClass('bounceInDown');
+                currentSlide = 1;
+                break;
+            case 3: 
+                $('.slide:nth-child(2)').removeClass('bounceOutLeft');
+                $('.slide:nth-child(2)').addClass('bounceInLeft');
+                currentSlide = 2;
+                break;            
+        }
+
+      } else {
+        console.log('down' + currentSlide);
+
+        $('.slide').removeClass('bounceInUp');
+
+        switch (currentSlide) {
+            case 1: 
+                $('.slide:nth-child(1)').addClass('bounceOutUp');
+                currentSlide = 2;
+                break;
+            case 2: 
+                $('.slide:nth-child(2)').addClass('bounceOutLeft');
+                currentSlide = 3;
+                break;            
+        }
+      }        
     }
 
-    setTimeout(function() {
-      $('.slide').removeClass('bounceInUp bounceInRight');
-    }, 1000);
-	});
+    
+
 });
